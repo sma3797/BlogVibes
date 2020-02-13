babbf70124887c4034ab12d09f9df0bcc8db22b9
sma3797 <sma3797@gmail.com>
2020-02-13 13:35:37 +0500

    Navigation Bar Done

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// We listen to the resize event
+window.addEventListener("resize", () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-items");
+  const nav2 = document.querySelector(".nav-social-icons");
+  const navItems = document.querySelectorAll(".nav-items li");
+  const navItems2 = document.querySelectorAll(".nav-social-icons a i");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    nav2.classList.toggle("nav-active");
+
+    burger.classList.toggle("toggle");
+
+    navItems.forEach((link, index) => {
+      if (link.style.animation) link.style.animation = "";
+      else
+        link.style.animation = `navItemsFade 0.5s ease forwards ${index / 7 +
+          0.3}s`;
+    });
+
+    navItems2.forEach((link, index) => {
+      if (link.style.animation) link.style.animation = "";
+      else
+        link.style.animation = `navItemsFade 0.5s ease forwards ${index / 7}s`;
+    });
+  });
+};
+
+navSlide();
