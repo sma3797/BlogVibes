a1a8cbd45f68ed495a817e14f5d0e05b9c944a79
sma3797 <sma3797@gmail.com>
2020-02-18 00:11:30 +0500

    Responsive done

--- js/script.js
+++ js/script.js
@@ -5,17 +5,33 @@ window.addEventListener("resize", () => {
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
+const responsive = {
+  0: {
+    items: 1
+  },
+  320: {
+    items: 1
+  },
+  560: {
+    items: 2
+  },
+  960: {
+    items: 3
+  }
+};
+
 $(document).ready(function() {
   $(".owl-carousel").owlCarousel({
     loop: true,
     autoplay: true,
     autoplayTimeout: 3000,
-    dots:false,
+    dots: false,
     nav: true,
     navText: [
       $(".owl-navigation .owl-nav-prev"),
       $(".owl-navigation .owl-nav-next")
-    ]
+    ],
+    responsive: responsive
   });
 });
 
@@ -47,4 +63,14 @@ const navSlide = () => {
   });
 };
 
+$(".moveUp").click(function() {
+  $("html, body").animate(
+    {
+      scrollTop: 0
+    },
+    1000
+  );
+});
+
+AOS.init();
 navSlide();
